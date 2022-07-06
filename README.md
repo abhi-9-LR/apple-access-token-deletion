20acd710e85d1ec869c17beb104e30febfae194f
Abhishek Saini <abhishek.saini@loginradius.com>
2022-07-06 17:35:38 +0530

    added images

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-test
+# Apple Access token deletion 
+This repository provides details on how to invalidate the apple access token based on LoginRadius AccountDeletion webhook event response https://www.loginradius.com/docs/integrations/webhook/#webhooks.
+
+## Description
+This respository provides an (POST) API endpoint which accepts the LoginRadius AccountDeletion webhook event response.
+
+For the Sample AccountDeletion webhook event response please see https://www.loginradius.com/docs/integrations/webhooks/samples/#deleteaccount9
+
+## Installation
+Run this command to install the required dependency.
+``` bash
+npm install 
+```
+Create a `.env` file in root directory and add following constant
+ 1. Create an environment variable `client_id` and add the apple client_id, which will be used in apple revoke token API call.
+ 2. Create an environment variable `client_secret` and add the apple client_secret, which will be used in apple revoke token API call.
+ For insights on how to create apple client_secret, please see https://github.com/LoginRadius/apple-client-secret-generator
+
+Run this command to start the server
+```bash
+npm start
+```
+
+### Apple access token deletion endpoint API.
+ <br>
+ This Api revokes the apple access token received from the user profile
+
+ #### Api Endpoint
+ `http://localhost:3000/api/webhook`
+
+### Sample Request Payload
+
+![](assets/postmanCall.png)
+
+
+Sample respone: 
+
+#### Output
+```json
+{
+    "description": "",
+    "statusCode": 200
+}
+
+Note: On success, Apple only returns HTTP code as 200 and does not return any success message/description
+```
+
+### Common Error: 
+```json
+{
+    "message": "{\"error\":\"invalid_client\"}",
+    "description": "",
+    "statusCode": 400,
+    "stackTrace": "Error: got 400 response\n    at generateErrorMessage ....."
+}
+
+Note: It signifies Apple provided `client_id` and `client_secret` are incorrect/expired.
+```
+
+Note: this script consider your apple social provide name as `apple`, if for your LoginRadius App, it social provider is configured with different name then e.g apple_brand1 please replace the same in the /api/webHookController.js file. ScreenShot reference: 
+
+![](assets/providerName.png)
--- api/controller/webHookController.js
+++ api/controller/webHookController.js
@@ -3,76 +3,75 @@ const unirest = require('unirest');
 var webhookPayload = (req, res, next) => {
 
     try {
-    let webhookResponse = req.body ? (req.body.Data ? (req.body.Data.Identities ? req.body.Data.Identities : null) : null) : null;
+        let webhookResponse = req.body ? (req.body.Data ? (req.body.Data.Identities ? req.body.Data.Identities : null) : null) : null;
         let appleAccessToken;
-if (webhookResponse) {
-        for (key in webhookResponse) {
-            if (webhookResponse[key].Provider = 'apple') {
-              appleAccessToken = webhookResponse[key].ProviderAccessCredential ? (webhookResponse[key].ProviderAccessCredential.AccessToken ?webhookResponse[key].ProviderAccessCredential.AccessToken: null ) : null
-            }
-        }
-    }
-    if (!appleAccessToken) {
-        let errorResponse = {
-            message: 'Access Token is required',
-            description: 'Apple Access token not found in the user webhook response',
-            statusCode : 403
-        };
-
-        res.status(403).json(errorResponse)
-    } else {
-
-
-        let element = {
-            appleEndpoint: "https://appleid.apple.com/auth/revoke"
-        }
-        let payload = {
-
-            client_id: process.env.client_id,
-            client_secret: process.env.client_secret,
-            token: appleAccessToken,
-            token_type_hint: "access_token"
-        }
-        var client = unirest.post(element.appleEndpoint)
-            .headers({
-                'Content-Type': 'application/x-www-form-urlencoded'
-            })
-            .send(payload);
-
-
-        client.end(function (response) {
-            if (response.error) {
-                var errorResponse = {
-                    message: JSON.stringify(response.body),
-                    description: '',
-                    statusCode: response.status || 200,
-                    stackTrace: response.error.stack,
-                    requestPath: element.Url
-                };
-                console.log(errorResponse);
-
-                res.status(errorResponse.statusCode).json(errorResponse)
-            } else {
-                var successResponse = {
-                    message: JSON.stringify(response.body),
-                    description: '',
-                    statusCode: response.status || 200
-
+        if (webhookResponse) {
+            for (key in webhookResponse) {
+                if (webhookResponse[key].Provider = 'apple') {
+                    appleAccessToken = webhookResponse[key].ProviderAccessCredential ? (webhookResponse[key].ProviderAccessCredential.AccessToken ? webhookResponse[key].ProviderAccessCredential.AccessToken : null) : null
                 }
-                console.log(successResponse);
-                res.status(successResponse.statusCode).json(successResponse)
             }
-        });
-    }
-} catch(err){
-    let errorResponse = {
-        message: JSON.stringify(err),
-        description: '',
-        statusCode: err.status || 500
+        }
+        if (!appleAccessToken) {
+            let errorResponse = {
+                message: 'Access Token is required',
+                description: 'Apple Access token not found in the user webhook response',
+                statusCode: 403
+            };
+
+            res.status(403).json(errorResponse)
+        } else {
+
+
+            let element = {
+                appleEndpoint: "https://appleid.apple.com/auth/revoke"
+            }
+            let payload = {
+
+                client_id: process.env.client_id,
+                client_secret: process.env.client_secret,
+                token: appleAccessToken,
+                token_type_hint: "access_token"
+            }
+            var client = unirest.post(element.appleEndpoint)
+                .headers({
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                })
+                .send(payload);
+
+
+            client.end(function (response) {
+                if (response.error) {
+                    var errorResponse = {
+                        message: JSON.stringify(response.body),
+                        description: '',
+                        statusCode: response.status || 200,
+                        stackTrace: response.error.stack,
+                        requestPath: element.Url
+                    };
+
+                    res.status(errorResponse.statusCode).json(errorResponse)
+                } else {
+                    var successResponse = {
+                        message: JSON.stringify(response.body),
+                        description: '',
+                        statusCode: response.status || 200
+
+                    }
+                    res.status(successResponse.statusCode).json(successResponse)
+                }
+            });
+        }
+    } catch (err) {
+        let errorResponse = {
+            message: JSON.stringify(err),
+            description: '',
+            statusCode: err.status || 500
         };
 
-    res.status(errorResponse.statusCode).json(errorResponse)
+        res.status(errorResponse.statusCode).json(errorResponse)
 
-}};
+    }
+};
 
 module.exports = { webhookPayload }
